5184d77c1203a72a80b50115bf12bc3861da49f6
Donovan Broquin <donovan@broquin.dev>
2026-08-24 14:28:47 +0200

    chore: update Vite configuration to work out of the box

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,3 +18,47 @@ laravel new application --bun --no-boost
 # Copy fresh app into /app
 cp -a "$bootstrap_dir/application"/. /app/
 cd /app
+
+# Update Vite config for Docker environment
+vite_config=""
+if [ -f vite.config.ts ]; then
+    vite_config=vite.config.ts
+elif [ -f vite.config.js ]; then
+    vite_config=vite.config.js
+fi
+
+if [ -n "$vite_config" ] && ! grep -q "host: '0.0.0.0'" "$vite_config"; then
+    vite_config_tmp="$(mktemp)"
+
+    if grep -q "server:[[:space:]]*{" "$vite_config"; then
+        awk '
+            !inserted && /^[[:space:]]*server:[[:space:]]*{/ {
+                print
+                print "        host: '\''0.0.0.0'\'',"
+                print "        hmr: {"
+                print "            host: '\''127.0.0.1'\''"
+                print "        },"
+                print "        cors: true,"
+                inserted = 1
+                next
+            }
+            { print }
+        ' "$vite_config" > "$vite_config_tmp"
+    else
+        awk '
+            { print }
+            !inserted && /export default defineConfig\(\{/ {
+                print "    server: {"
+                print "        host: '\''0.0.0.0'\'',"
+                print "        hmr: {"
+                print "            host: '\''127.0.0.1'\''"
+                print "        },"
+                print "        cors: true,"
+                print "    },"
+                inserted = 1
+            }
+        ' "$vite_config" > "$vite_config_tmp"
+    fi
+
+    mv "$vite_config_tmp" "$vite_config"
+fi
